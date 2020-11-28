73b6eb89b037071764ff7bb68aa4f4840bada768
middleverse <arshi_b@hotmail.com>
2020-11-28 14:27:27 -0500

    simplified symbols to make for easier hacking

--- substitutionCipher.py
+++ substitutionCipher.py
@@ -1,22 +1,11 @@
+# ===================
+# Substitution Cipher
+# ===================
+
 from affineCipher import encrypt
 import random
 
-SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.-'
-
-def main():
-    message = 'This is a run of the substitution cipher'
-    letters = list(SYMBOLS) # generate random key
-    random.shuffle(letters)
-    key = ''.join(letters)
-
-    mode = 'encrypt'
-    if mode == 'encrypt':
-        translated = encrypt(key, message)
-    elif mode == 'decrypt':
-        translated = decrypt(key, message)
-    print('Using key: %s' % (key))
-    print('The %sed message is:' % (mode))
-    print(translated)
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def encrypt(key, plaintext):
     return translatedMessage(key, plaintext, 'encrypt')
@@ -33,12 +22,30 @@ def translatedMessage(key, message, mode):
         charsA, charsB = charsB, charsA
     # translate each symbol in message, adding it to translated string
     for symbol in message:
-        if symbol in charsA:
-            symbolIndex = charsA.find(symbol)
-            translated += charsB[symbolIndex]
+        if symbol.upper() in charsA:
+            symbolIndex = charsA.find(symbol.upper())
+            if symbol.isupper():
+                translated += charsB[symbolIndex].upper()
+            else:
+                translated += charsB[symbolIndex].lower()
         else:
             translated += symbol
     return translated
+
+def main():
+    message = 'This is a run of the substitution cipher'
+    letters = list(SYMBOLS) # generate random key
+    random.shuffle(letters)
+    key = ''.join(letters)
+
+    mode = 'encrypt'
+    if mode == 'encrypt':
+        translated = encrypt(key, message)
+    elif mode == 'decrypt':
+        translated = decrypt(key, message)
+    print('Using key: %s' % (key))
+    print('The %sed message is:' % (mode))
+    print(translated)
 
 if __name__ == '__main__':
     main()
