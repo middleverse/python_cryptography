2731695ad4aacc9128a5687afbcad0a81ea1f663
middleverse <arshi_b@hotmail.com>
2020-12-02 10:10:50 -0500

    complete

--- vigenereCipher.py
+++ vigenereCipher.py
@@ -4,17 +4,48 @@
 
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+def translateMessage(key, message, mode):
+    translated = [] # hold translated message here
+    keyIndex = 0
+    key = key.upper() # for simplicity
+
+    # for each symbol, translate it with keyIndex
+    # if encrypting, add the keyIndex
+    # if decrypting, subtract the keyIndex
+    for symbol in message:
+        symbolIndex = LETTERS.find(symbol.upper())
+        # if symbol is in LETTERS, translate
+        if symbolIndex != -1:
+            if mode == 'encrypt':
+                symbolIndex += LETTERS.find(key[keyIndex])
+            elif mode =='decrypt':
+                symbolIndex -= LETTERS.find(key[keyIndex])
+            symbolIndex %= len(LETTERS) # wrap around
+            # append with appropriate case
+            if symbol.isupper():
+                translated.append(LETTERS[symbolIndex].upper())
+            else:
+                translated.append(LETTERS[symbolIndex].lower())
+            # move key to the next letter
+            keyIndex = (keyIndex + 1) % len(key)
+
+        # otherwise append as-is
+        else:
+            translated.append(symbol)        
+    return ''.join(translated) # convert list into str
+
 
 def encrypt(key, plaintext):
-    return -1
+    return translateMessage(key, plaintext, 'encrypt')
 
 def decrypt(key, ciphertext):
-    return -1
+    return translateMessage(key, ciphertext, 'decrypt')
 
 def main():
-    message = '''Alan Mathison Turing was a British mathematician, logician, cryptanalyst, and computer scientist.'''
+    # "Alan Mathison Turing was a British mathematician, logician, cryptanalyst, and computer scientist."
+    message = '''Adiz Avtzqeci Tmzubb wsa m Pmilqev halpqavtakuoi, lgouqdaf, kdmktsvmztsl, izr xoexghzr kkusitaaf.'''
     key = 'ASIMOV'
-    mode = 'encrypt'
+    mode = 'decrypt'
 
     if mode == 'encrypt':
         translated = encrypt(key, message)
